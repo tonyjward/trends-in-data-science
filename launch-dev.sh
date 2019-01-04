e16d035db4885d4c8a12e5894b02f39a08dfa758
Tony Ward <tony.ward@ukpowernetworks.co.uk>
2019-01-04 16:22:03 +0000

    Fix reference to correct docker image

--- launch-dev.sh
+++ launch-dev.sh
@@ -4,6 +4,5 @@
 #-v $(pwd):/home/rstudio/ \
 #-e PASSWORD=letmein \
 #tonyjward/rstudio:latestbuild
-
-sudo docker run --rm -d -p 80:8787 -e PASSWORD=letmein -v $(pwd):/home/rstudio tonyjward/rstudio:latestbuild
+sudo docker run --rm -d -p 80:8787 -e PASSWORD=letmein -v $(pwd):/home/rstudio tonyjward/rstudio:dev
 
