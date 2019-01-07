a1f919dff161ca36b0c2c384de913bae446f46ca
Tony Ward <tony.ward@ukpowernetworks.co.uk>
2019-01-07 16:18:27 +0000

    Format topic probability matrix

--- App/modules/topicProb.R
+++ App/modules/topicProb.R
@@ -1,5 +1,19 @@
 topicProb <- function(input, output, session, inputData){
   output$table <- DT::renderDataTable({
-    datatable(inputData)
+    
+    columnNames <- c("text_field",grep("Topic", colnames(inputData),value = TRUE))
+    
+    datatable(
+      inputData[, ..columnNames],
+      filter = "top",
+      options = list(
+        autoWidth = TRUE,
+        scrollX = TRUE, # required to change column length https://github.com/rstudio/DT/issues/29
+        columnDefs = list(list(width = '1000px', targets = 1)))
+    )
   }) 
 }
+
+
+
+
