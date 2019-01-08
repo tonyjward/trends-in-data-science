a43ddd3454399205935b917e03b756d941b30670
Tony Ward <tony.ward@ukpowernetworks.co.uk>
2019-01-08 16:59:08 +0000

    Remove unnecesary code

--- App/modules/tools.R
+++ App/modules/tools.R
@@ -3,10 +3,6 @@ tools <- function(input, output, session, inputData){
   # Time Series Bar Plot
   output$plot <- renderPlot({
     
-    inputData[, Tools := ifelse(Python_R == "TRUE_TRUE", "Python or R",
-                         ifelse(Python_R == "TRUE_FALSE", "Python but not R",
-                                ifelse(Python_R == "FALSE_TRUE", "R but not Python","Neither Language")))]
-    
     plotData <- inputData[,.N, .(Tools, job_type)]
     
     ggplot(data = plotData,
