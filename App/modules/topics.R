7124700dece1666e3679ea65b30e45051d843c40
Tony Ward <tony.ward@ukpowernetworks.co.uk>
2019-01-08 16:29:24 +0000

    Remove column rename since this is done prior to shiny

--- App/modules/topics.R
+++ App/modules/topics.R
@@ -5,15 +5,9 @@ topics <- function(input, output, session, inputData){
     
     topicNames <- grep("Topic", colnames(inputData), value = TRUE)
     
-    newNames <- paste0(topWords$topWords," (", topicNames, ")")
-    
-    setnames(inputData,
-             old = topicNames,
-             new = newNames)
-    
     moltenData <- melt(data = inputData,
                        id.vars = "job_type",
-                       measure.vars = newNames)
+                       measure.vars = topicNames)
     
     ggplot(data = moltenData,
            aes(x = variable , y = value, fill = job_type)) + geom_bar(stat = "identity") + coord_flip() + labs(x = "Topic", y = "Sum of Topic Probabilities")
