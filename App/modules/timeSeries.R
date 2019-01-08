93c3e7e85d192e339d8564318a56b9e1ab357750
Tony Ward <tony.ward@ukpowernetworks.co.uk>
2019-01-08 16:58:38 +0000

    Enhance plot with informative variable name

--- App/modules/timeSeries.R
+++ App/modules/timeSeries.R
@@ -2,7 +2,7 @@ timeSeries <- function(input, output, session, jobData){
   
   # Time Series Bar Plot
   output$plot <- renderPlot({
-    ggplot(data = jobData, aes_string(x = "yearMonDay", fill = "Python_R")) + "geom_bar"()
+    ggplot(data = jobData, aes_string(x = "yearMonDay", fill = "Tools")) + "geom_bar"()
   })
   
   
