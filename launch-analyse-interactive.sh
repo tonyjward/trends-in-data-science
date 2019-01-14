fa9515c78dcb98ccde3ea1123b6bf389089b788f
Tony Ward <tony.ward@ukpowernetworks.co.uk>
2019-01-14 15:09:59 +0000

    Change ports

--- launch-analyse-interactive.sh
+++ launch-analyse-interactive.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 sudo docker stop $(sudo docker ps -aq) && \
-sudo docker run --rm -d -p 80:8787 -e PASSWORD=letmein -v $(pwd):/home/rstudio tonyjward/rstudio:analyseinteractive
+sudo docker run --rm -d -p 443:8787 -e PASSWORD=letmein -v $(pwd):/home/rstudio  tonyjward/rstudio:analyseinteractive
 
