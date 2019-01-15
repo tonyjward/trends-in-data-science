b738dc766d027b2fa7ab9109dd98870d078c16da
Tony Ward <tony.ward@ukpowernetworks.co.uk>
2019-01-15 09:29:39 +0000

    Ensure RStudio cache is cleared prior to launch

--- launch-scraping-interactive.sh
+++ launch-scraping-interactive.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 sudo docker stop $(sudo docker ps -aq) && \
+rm -rf /home/d14xj1/repos/trends-in-data-science/.rstudio && \ # clear Rstudio cache
 sudo docker-compose -f docker-compose-interactive.yml up --force-recreate -d
--- launch-scraping.sh
+++ launch-scraping.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 sudo docker stop $(sudo docker ps -aq) && \
+rm -rf /home/d14xj1/repos/trends-in-data-science/.rstudio && \ # clear Rstudio cache
 sudo docker-compose up --force-recreate -d
