274e7fa1a059825e673b2ad0c37344ff181c1cc6
Tony Ward <tony.ward@ukpowernetworks.co.uk>
2019-01-31 06:18:43 +0000

    Change ports for shiny app

--- launch-shiny.sh
+++ launch-shiny.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 sudo docker stop $(sudo docker ps -aq)
-sudo docker run -p 80:3838 -d -v $(pwd)/App:/srv/shiny-server/ tonyjward/rstudio:shiny 
+sudo docker run -p 443:3838 -d -v $(pwd)/App:/srv/shiny-server/ tonyjward/rstudio:shiny 
 
