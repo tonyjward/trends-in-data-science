bfcff8a7d2e3e17e2ba4043844782e056eb844db
tonyjward <tony@statcore.co.uk>
2020-01-10 18:04:42 +0000

    Display topic probs in long format

--- Shiny/modules/topicProb.R
+++ Shiny/modules/topicProb.R
@@ -2,18 +2,18 @@ topicProb <- function(input, output, session, inputData){
   output$table <- DT::renderDataTable({
     
     # rename column names e.g. Topic1 becomes "1"
-    topicNames <- grep("Topic", colnames(inputData()),value = TRUE)
+    #topicNames <- grep("Topic", colnames(inputData()),value = TRUE)
 
     
-    displayNames <- c("text_field",topicNames)
+    #displayNames <- c("text_field",topicNames)
     
     datatable(
-      inputData()[order(-`Posted Date`), ..displayNames],
+      inputData()[order(-`Posted Date`)],
       filter = "top",
       options = list(
         autoWidth = TRUE,
         scrollX = TRUE, # required to change column length https://github.com/rstudio/DT/issues/29
-        columnDefs = list(list(width = '1000px', targets = 1)))
+        columnDefs = list(list(width = '1000px', targets = 4)))
     )
   }) 
 }
