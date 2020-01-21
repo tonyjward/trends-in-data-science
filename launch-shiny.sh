85b6139bfadb5a9548e77f17665094a5767b0ac2
Tony Ward <tony@statcore.co.uk>
2020-01-21 11:18:41 +0000

    Display app on apps.statcore.co.uk with ssl encrpytion!

--- launch-shiny.sh
+++ launch-shiny.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 sudo docker stop $(sudo docker ps -aq)
-sudo docker run -p 80:3838 -d -v $(pwd)/Shiny:/srv/shiny-server/ tonyjward/rstudio:shiny 
-
+sudo docker-compose -f docker-compose-shiny-nginx.yml up --force-recreate -d
