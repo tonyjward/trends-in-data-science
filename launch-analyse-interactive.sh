b8f65dec277756e01bd1adc9dadaf88242c4b847
Tony Ward <tony@statcore.co.uk>
2020-01-21 15:18:56 +0000

    Add missing libraries

--- launch-analyse-interactive.sh
+++ launch-analyse-interactive.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 sudo docker stop $(sudo docker ps -aq) && \
 rm -rf /home/d14xj1/repos/trends-in-data-science/.rstudio && \ # clear Rstudio cache
-sudo docker run --rm -d -p 443:8787 -e PASSWORD=letmein -v $(pwd):/home/rstudio  tonyjward/rstudio:analyseinteractive
+sudo docker run --rm -d -p 3838:8787 -e PASSWORD=letmein -v $(pwd):/home/rstudio  tonyjward/rstudio:analyseinteractive
 
