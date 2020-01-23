395281f43f23491688ae6e0d452f0741b17276a7
Tony Ward <tony@statcore.co.uk>
2020-01-23 21:08:20 +0000

    Fix legend

--- Shiny/modules/timeSeriesOverall.R
+++ Shiny/modules/timeSeriesOverall.R
@@ -2,8 +2,8 @@ timeSeriesOverall <- function(input, output, session, inputData){
   
   # Time Series Bar Plot
   output$plot <- renderPlot({
-    ggplot(data = inputData, aes(x = month, y = N)) + geom_line(aes(colour = "blue")) + scale_x_date(labels = scales::date_format("%Y-%m"),
-                                                                                                       breaks = "1 month")
+    ggplot(data = inputData, aes(x = month, y = N)) + geom_line() + 
+      scale_x_date(labels = scales::date_format("%Y-%m"), breaks = "1 month")
   })
   
 }
