de49765e6488adc78341d97b6aa3003792b2aa92
tonyjward <tony@statcore.co.uk>
2020-01-24 17:01:26 +0000

    Add job_type radio button to location

--- Shiny/modules/locationSplit.R
+++ Shiny/modules/locationSplit.R
@@ -4,14 +4,14 @@ locationSplit <- function(input, output, session, inputData, aggData){
   # Roles
   output$plotRoles <- renderPlot({
 
-    ggplot(data = plotData[job_type == "Contract"],
+    ggplot(data = plotData[job_type == input$job_type],
            aes(x = London, y = N, fill = London))  + geom_bar(stat = "identity") + labs(y = "Job Count")
   })
   
   # Pay
   output$plotPay <- renderPlot({
     
-    ggplot(data = inputData[job_type == "Permanent"], aes(x = London, y = salaryMax, fill = London)) + geom_boxplot() +
+    ggplot(data = inputData[job_type == input$job_type], aes(x = London, y = salaryMax, fill = London)) + geom_boxplot() +
       scale_y_continuous(limits = c(0, NA),
                          breaks = scales::pretty_breaks(n = 10)) 
   })
--- Shiny/modules/locationSplitUI.R
+++ Shiny/modules/locationSplitUI.R
@@ -2,6 +2,10 @@ locationSplitUI <- function(id){
   ns <- shiny::NS(id)
   
   tagList(
+    radioButtons(ns("job_type"), label = h3("Select Job Type"),
+                 choices = list("Permanent", "Contract"), 
+                 selected = "Permanent"),
+    br(),
     h3("Roles"),
     plotOutput(ns("plotRoles")),
     br(),
