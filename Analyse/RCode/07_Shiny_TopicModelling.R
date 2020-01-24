b5d41ddb9ef3fc50fb679655a2bfdb1025edff92
tonyjward <tony@statcore.co.uk>
2020-01-24 15:37:46 +0000

    Tidy up

--- Analyse/RCode/07_Shiny_TopicModelling.R
+++ Analyse/RCode/07_Shiny_TopicModelling.R
@@ -18,25 +18,17 @@ load(file = file.path(dirRData, '03_dt_all.RData'))
 
 
 outputData<- lapply(names(fitted_many_p), function(x) {
-  # LDA_fit <- fitted_many_p[[1]]
+  # LDA_fit <- fitted_many_p[[1]] 
   
   LDA_fit <- fitted_many_p[[x]]
   
-  # VISUALISE TOPICS
-  # Note that we use the just the corpus and document term matrix from the training data
-  # as this was the data used to train the model
-  
   print(LDA_fit@k)
   topicsizes <- LDA_fit@k
-
+  
+  # required for ldaVis
   jsonviz <- topicJson(LDA_fit, txtCorpus, txtDtm)
   x <- fromJSON(jsonviz)
 
-  
-  # OBTAIN TOPIC PROBABILITIES
-  # Note that we score the entire data (both training and validation datasets) and then
-  # add a column indicating whether the row is in the training or validation set
-  
   # posterior topic probabilities
   topic_probsAll <- posterior(LDA_fit,newdata = txtDtm)$topics # or use LDA_fit@gamma which "should" give same results but doesn't
   
@@ -65,21 +57,11 @@ outputData<- lapply(names(fitted_many_p), function(x) {
   topicNames <- grep("Topic", colnames(topic_probsAll), value = TRUE)
   colnames(topic_probsAll) <- paste0(top_words$topWords," (", topicNames, ")")
   
-  # export topic probabilities, topic assignments and raw text field to csv file
-  outputAll=data.table(round(100*topic_probsAll),
+  # produce topic probabilities in long format
+  outputAll <- data.table(round(100*topic_probsAll),
                        doc_id = dt_all[,doc_id],
                        text_field = dt_all[,text], 
                        partition = ifelse(dt_all[["fold"]] %in% train_folds,"TRAIN","VALID"))
-  
-  
-  # replace line endings for all character variables so we can export
-  # characterIdx <- sapply(outputAll, is.character)
-  # characterNames <- names(characterIdx)[characterIdx]
-  # 
-  # outputAll[, (characterNames):= lapply(.SD, function(x){
-  #   gsub("[\r\n]", "", x)
-  # }), .SDcols = characterNames]
-  
   outputMolten <- melt(outputAll, 
                        id.vars = c("doc_id", "text_field"), 
                        measure.vars = grep(pattern = 'Topic', colnames(outputAll), value = TRUE),
