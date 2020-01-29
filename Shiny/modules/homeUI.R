db15b85218d7d3a9c3c93fb7f3926e387700aacc
tonyjward <tony@statcore.co.uk>
2020-01-29 16:38:08 +0000

    Update homepage

--- Shiny/modules/homeUI.R
+++ Shiny/modules/homeUI.R
@@ -1,29 +1,18 @@
 homeUI <- function(){
   tagList(
     h1("Trends in Data Science"),
-    "This web application tracks trends in the permanent and contract data science job market.",
-    tags$br(),
-    tags$br(),
     img(src = "datascience.png", width = 500, height = 400),
     tags$hr(),
-    h2("Instructions & Source Code"),
-    "Instructions on how to use the app can be found at this",
-    tags$a(href="https://github.com/tonyjward/trends-in-data-science", "medium article"),
-    tags$br(),
-    "Full source code available at this ",
-    tags$a(href="https://github.com/tonyjward/trends-in-data-science", "github repo"),
-    tags$br(),
-    "If you find this app useful please give some stars/claps :)",
-    tags$hr(),
-    h2("Project Description"),
-    "The data source for this project is jobserve.co.uk. On a schedule (daily) we perform the following",
+    h2("About"),
+    "This web application tracks trends in the permanent and contract data science job market.",
+    "On a schedule (currently daily) we perform the following",
     tags$ol(
-      tags$li("Scrape all 'Data Scientist' jobs from jobserve"),
+      tags$li("Scrape all 'Data Scientist' jobs from jobserve.co.uk"),
       tags$li("Pre-process data, produce visualisations and build topic models on the job description"),
-      tags$li("Present output using an interactive web application")
+      tags$li("Present output using an this interactive Shiny application")
     ),
     tags$hr(),
-    h2("App Navigation"),
+    h2("Navigating the App"),
     h4("Data"),
     "Select between Permanent and Contract roles then explore historical job postings.",
     "Use the filters at the top to perform keyword search, or use arrows to sort data by salary or posted date.",
@@ -39,7 +28,15 @@ homeUI <- function(){
     h4("Location"),
     "A comparison between London and the rest of the UK.",
     h4("Trends"),
-    "Monitor the monthly trends for both job type (Perm/Contract) and tools (Python/R)."
+    "Monitor the monthly trends for both job type (Perm/Contract) and tools (Python/R).",
+    tags$hr(),
+    h2("Source Code"),
+    "Full source code available on ",
+    tags$a(href="https://github.com/tonyjward/trends-in-data-science", "github"),
+    tags$hr(),
+    h2("App Author"),
+    "Tony Ward -  ",
+    tags$a(href="https://statcore.co.uk/", "freelance data science consultant")
     
   )
 }
