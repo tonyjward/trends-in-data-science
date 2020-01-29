e484cbdfd6305bd1dd7c37e487560d7e47d336b5
Tony Ward <tony@statcore.co.uk>
2020-01-29 14:41:49 +0000

    Update docker build shell scripts

---
+++ Docker/build-analyse-interactive.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+sudo docker build -f DockerfileAnalyseInteractive -t tonyjward/trends-in-data-science:analyseinteractive . && \
+
--- Docker/build-analyse.sh
+++ Docker/build-analyse.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-sudo docker build -f DockerfileAnalyseInteractive -t tonyjward/trends-in-data-science:analyseinteractive . && \
 sudo docker build -f DockerfileAnalyse -t tonyjward/trends-in-data-science:analyse .
